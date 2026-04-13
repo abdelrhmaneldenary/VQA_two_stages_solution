c98586b2989d4470d7e7059709c36dab5708e3b1
Abdelrhman Eldenary <abdelrhmaneldenary@gmail.com>
2026-04-13 21:38:52 +0200

    new simple arch

--- src/config.py
+++ src/config.py
@@ -6,8 +6,8 @@ BASE_DATA_DIR = "/kaggle/input/datasets/abdelrhmanshaheen/answer-therapy" if IS_
 CONFIG = {
     "run_id": "EXP_QWEN25_SAM3_SIMPLE",
     "notes": "Simple Qwen2.5-VL + SAM3 IoU baseline",
-    "model_s1_path": "/kaggle/input/datasets/ruhul77/qwen2-5-vl-3b-instruct",
-    "model_s2_path": "/kaggle/input/datasets/abdelrhmaneldenary/sam3-h"  ,
+    "model_s1_path": "/kaggle/input/datasets/ruhul77/qwen2-vl-2b-instruct",
+    "model_s2_path": "/kaggle/input/datasets/abdelrhmaneldenary/sam3-official-weights-v3"  ,
     "iou_threshold": 0.5,
     "dataset_dir": BASE_DATA_DIR,
 }
